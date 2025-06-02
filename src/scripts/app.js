7c0eb760f4581c2312eaf2face7c651baedae559
Aurélien Feye <aurelien.feye@student.heaj.be>
2025-06-02 14:28:42 +0200

    mobile nav

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,9 @@
-console.info('Hello world');
+"use strict";
+
+var menuBtn = document.querySelector(".menu--burger");
+menuBtn.addEventListener("click", openMenu);
+
+function openMenu(){
+  var menu = document.querySelector(".menu--links");
+  menu.classList.toggle("menu--links__hidden");
+  }
