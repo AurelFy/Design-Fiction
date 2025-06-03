9e6e23c9cba739ab87e610d4ca3f236cda13d233
Aurélien Feye <aurelien.feye@student.heaj.be>
2025-06-03 17:45:36 +0200

    home page + start train page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,8 @@
 "use strict";
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 var menuBtn = document.querySelector(".menu--burger");
 menuBtn.addEventListener("click", openMenu);
 
@@ -7,3 +10,19 @@ function openMenu(){
   var menu = document.querySelector(".menu--links");
   menu.classList.toggle("menu--links__hidden");
   }
+
+/* gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray("p").forEach(p => {
+  gsap.from(p, {
+    scrollTrigger: {
+      trigger: p,
+      start: "top 80%", 
+      toggleActions: "restart reset restart reset"
+    },
+    opacity: 0,
+    x: -30,
+    duration: 1,
+    ease: "power2.out"
+  });
+}); */ 
